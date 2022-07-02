063982eb484d4540866bfbd0e16557614aaac4d2
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-07-01 21:16:18 -0400

    no art maybe use api

--- screens/Artists.js
+++ screens/Artists.js
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 
 const Artists = () => {
     return (
         <View>
-            <Text>Artists</Text>
-            </View>
+            <FlatList
+                data={allMedia}
+                keyExtractor={item => item.fileLocation}
+                renderItem={({ item }) => (
+                    <LibraryInfo mediaData={item} saveSong={saveSong} getIndex={getIndex}/>
+                )}
+            />
+        </View>
     )
 }
 
